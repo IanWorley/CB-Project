809a6c8865df68e74a4e72f9e6e989b0f189c1e6
Ian Worley <ian@ianworley.xyz>
2023-12-02 21:45:47 -0600

    add graalvm again

---
+++ CB-Backend/src/main/java/edu/ucmo/cbbackend/config/RuntimeHints.java
@@ -0,0 +1,21 @@
+package edu.ucmo.cbbackend.config;
+
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportRuntimeHints;
+
+@ImportRuntimeHints(MyRuntimeHints.MyRuntimeHintsRegistrar.class)
+@Configuration
+class MyRuntimeHints {
+
+    static class MyRuntimeHintsRegistrar implements RuntimeHintsRegistrar {
+
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            // Register resources
+            hints.resources().registerPattern("jwtprivate.pem");
+            hints.resources().registerPattern("jwtpub.pem");
+        }
+    }
+}
